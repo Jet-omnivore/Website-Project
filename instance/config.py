3899515cc8fadc538c5de9cb3d8401a5d0c70e31
Jet-omnivore <73430778+Jet-omnivore@users.noreply.github.com>
2022-02-08 10:03:16 +0530

    Add files via upload

---
+++ instance/config.py
@@ -0,0 +1,15 @@
+SECRET_KEY = '4b05e961b0a6f144a77f3f2f97910e4432e5ff4b72e05d03b02fc609883c0c1f'
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'yourpocketmoney.in@gmail.com'
+MAIL_PASSWORD = '9upju8jvr'
+
+MERCHANT_ID = "nbWKyo33717239359222"
+MERCHANT_KEY = "#rLXvpWQw0et2y#5"
+WEBSITE_NAME = "WEBSTAGING"
+INDUSTRY_TYPE_ID = "Retail"
+BASE_URL = "https://securegw.paytm.in"
+CALLBACK_URL = "http://127.0.0.1:5000/user/callback"
